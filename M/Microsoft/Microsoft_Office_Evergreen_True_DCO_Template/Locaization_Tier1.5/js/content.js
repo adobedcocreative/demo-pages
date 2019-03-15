8147efd31635729b9819ec33f6b069c60d5afaeb
Neeraj Deshmukh <ndeshmuk@adobe.com>
2019-03-15 09:56:39 +0530

    Microsoft Update

--- M/Microsoft/Microsoft_Office_Evergreen_True_DCO_Template/Locaization_Tier1.5/js/content.js
+++ M/Microsoft/Microsoft_Office_Evergreen_True_DCO_Template/Locaization_Tier1.5/js/content.js
@@ -24,9 +24,40 @@ var feedTemplate1 = [], feedTemplate2 = [], feedTemplate3 = [], feedTemplate4 = 
 var feedContent = [], feedData = {}, getData;
 var adData = [];
 var loadTemplateFlag1 = false, loadTemplateFlag2 = false, loadTemplateFlag3 = false, loadTemplateFlag4 = false;
-var getFeed2 = function(){
+var getFeed1 = function(){
   var xmlhttp = new XMLHttpRequest();
   var url = "https://spreadsheets.google.com/feeds/list/1Ck0yMKDz1MnIT_f6y-kN27im4k_m_esLs3-uDVl4f40/2/public/values?alt=json";
+
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          var JSONData = JSON.parse(this.responseText);
+          JSONData.feed.entry.map(function(data){
+            feedTemplate1.push({
+              "Ad Size": data['gsx$adsize']['$t'],
+              "CTA": data['gsx$cta']['$t'],
+              "Country": data['gsx$country']['$t'],
+              "Language": data['gsx$language']['$t'],
+              "Layout": data['gsx$layout']['$t'],
+              "Segment": data['gsx$segment']['$t'],
+              "Smart Names": data['gsx$smartnames']['$t'],
+              "costline1": data['gsx$costline1']['$t'],
+              "costline2": data['gsx$costline2']['$t'],
+              "costline3": data['gsx$costline3']['$t'],
+              "headline1": data['gsx$headline1']['$t'],
+              "headline2": data['gsx$headline2']['$t'],
+            });
+          });
+          loadTemplateFlag1 = true;
+          loadData();
+      }
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
+getFeed1();
+var getFeed2 = function(){
+  var xmlhttp = new XMLHttpRequest();
+  var url = "https://spreadsheets.google.com/feeds/list/1Ck0yMKDz1MnIT_f6y-kN27im4k_m_esLs3-uDVl4f40/3/public/values?alt=json";
 
   xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
@@ -57,7 +88,7 @@ var getFeed2 = function(){
 getFeed2();
 var getFeed3 = function(){
   var xmlhttp = new XMLHttpRequest();
-  var url = "https://spreadsheets.google.com/feeds/list/1Ck0yMKDz1MnIT_f6y-kN27im4k_m_esLs3-uDVl4f40/3/public/values?alt=json";
+  var url = "https://spreadsheets.google.com/feeds/list/1Ck0yMKDz1MnIT_f6y-kN27im4k_m_esLs3-uDVl4f40/4/public/values?alt=json";
 
   xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
@@ -85,9 +116,41 @@ var getFeed3 = function(){
   xmlhttp.send();
 }
 getFeed3();
+var getFeed4 = function(){
+  var xmlhttp = new XMLHttpRequest();
+  var url = "https://spreadsheets.google.com/feeds/list/1Ck0yMKDz1MnIT_f6y-kN27im4k_m_esLs3-uDVl4f40/5/public/values?alt=json";
+
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          var JSONData = JSON.parse(this.responseText);
+          JSONData.feed.entry.map(function(data){
+            feedTemplate4.push({
+              "Ad Size": data['gsx$adsize']['$t'],
+              "CTA": data['gsx$cta']['$t'],
+              "Country": data['gsx$country']['$t'],
+              "Language": data['gsx$language']['$t'],
+              "Layout": data['gsx$layout']['$t'],
+              "Segment": data['gsx$segment']['$t'],
+              "Smart Names": data['gsx$smartnames']['$t'],
+              "headline1": data['gsx$headline1']['$t'],
+              "headline2": data['gsx$headline2']['$t'],
+            });
+          });
+          loadTemplateFlag4 = true;
+          loadData();
+      }
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
+getFeed4();
 var loadData = function(){
-  if(loadTemplateFlag2 && loadTemplateFlag3) {
+  if(loadTemplateFlag1 && loadTemplateFlag2 && loadTemplateFlag3 && loadTemplateFlag4) {
     feedContent = [
+      {
+        name: 'Template 1',
+        feed: feedTemplate1,
+      },
       {
         name: 'Template 2',
         feed: feedTemplate2,
@@ -95,6 +158,10 @@ var loadData = function(){
       {
         name: 'Template 3',
         feed: feedTemplate3,
+      },
+      {
+        name: 'Template 4',
+        feed: feedTemplate4,
       },
     ]
     feedContent.map(function(feed){
