7cd6df3b891bbb429afbe100135a906616284bec
rajesh <rajeshku@adobe.com>
2019-12-19 23:23:37 +0530

    FIxed the issues

--- A/Altice/Altice_JanuaryNewBuild_2020/frames.js
+++ A/Altice/Altice_JanuaryNewBuild_2020/frames.js
@@ -1,1 +1,1 @@
-var publishVersion = 1576674530750, frames={"ads/SDL_BRD_AO_AO1Gig/300x250/300x250.html":{"frame1":"ads/SDL_BRD_AO_AO1Gig/300x250/300x250_frame1.html","frame2":"ads/SDL_BRD_AO_AO1Gig/300x250/300x250_frame2.html","frame3":"ads/SDL_BRD_AO_AO1Gig/300x250/300x250_frame3.html"}}
+var publishVersion = 1576774574398, frames={"ads/SDL_BRD_AO_AO1Gig/300x250/300x250.html":{"frame1":"ads/SDL_BRD_AO_AO1Gig/300x250/300x250_frame1.html","frame2":"ads/SDL_BRD_AO_AO1Gig/300x250/300x250_frame2.html","frame3":"ads/SDL_BRD_AO_AO1Gig/300x250/300x250_frame3.html"}}
