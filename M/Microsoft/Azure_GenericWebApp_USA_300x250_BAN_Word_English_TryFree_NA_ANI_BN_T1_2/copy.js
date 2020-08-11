c3f55199365d914c08b99afcde3325fb5c6cc1b6
Rajesh Kumar <rajeshku@adobe.com>
2020-08-11 11:21:19 +0530

    added old template

---
+++ M/Microsoft/Azure_GenericWebApp_USA_300x250_BAN_Word_English_TryFree_NA_ANI_BN_T1_2/copy.js
@@ -0,0 +1,14 @@
+	// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+
+	bannerData.headline1 = ["<#505050>Code, experiment, |and build your next |great web app","27px",15,52,"27","350", "left"];
+
+
+
+	bannerData.CTA = ["<#FFFFFF>Try Azure free","28px",0,0,"50","300", "center"];
+	
+	bannerData.CTAarrowVisible = [true, 0,0]
