489e2d235d662bf27f6f376cb8e4dc98fa3e5b78
Neeraj Deshmukh <ndeshmuk@adobe.com>
2020-09-23 19:57:24 +0530

    Microsoft Update

---
+++ M/Microsoft/M365_Interactive_Cube/ads/160x600/3DCube_Desktop/copy.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+
+
+	bannerData.headline1 = ['<#000000> ','22.5px',31,71,'28','350', 'left']
+
+	bannerData.headline1_1 = ['<#505050>Bring out |your best','32.6px',31.5,46,'35','350', 'left']
+	bannerData.headline1_2 = ['<#505050>More time for what |you love with premium |Office apps, 1 TB of |cloud storage, and |more in one easy |subscription','20.2px',31.5,133,'24.5','350', 'left']
+
+	bannerData.headline2_1 = ['<#505050>Share |memories |across |devices','31.8px',33,266,'32.5','350', 'left']
+
+	bannerData.headline2_2 = ['<#505050>Access your photos |anytime, anywhere','20px',33,414,'24.5','350', 'left']
+	bannerData.headline2_3 = ['<#505050>Organize, tag, and |find your favorite |memories','20px',33,473,'24.5','350', 'left']
+	bannerData.headline2_4 = ['<#505050>Keep your files |secured with |Personal Vault','20px',33,556,'24.5','350', 'left']
+	bannerData.headline2_5 = ['<#505050>+ more','20px',33,636,'24.5','350', 'left']
+	
+	bannerData.headline3_1 = ['<#505050>Be a hero |storyteller','31.8px',33,266,'37.5','350', 'left']
+
+	bannerData.headline3_2 = ['<#505050>Create quickly with |smart templates','20px',33,354,'24.5','350', 'left']
+	bannerData.headline3_3 = ['<#505050>Collaborate in real |time','20px',33,413,'24.5','350', 'left']
+	bannerData.headline3_4 = ['<#505050>Present on any |device','20px',33,472,'24.5','350', 'left']
+	bannerData.headline3_5 = ['<#505050>+ more','20px',33,531,'24.5','350', 'left']
+
+	bannerData.headline4_1 = ['<#505050>Create |memorable |moments','31.8px',33,266,'37.5','350', 'left']
+
+	bannerData.headline4_2 = ['<#505050>Make it memorable |with 3D images','20px',33,393,'24.5','350', 'left']
+	bannerData.headline4_3 = ['<#505050>Use smart tools for |writing tips','20px',33,451,'24.5','350', 'left']
+	bannerData.headline4_4 = ['<#505050>Turn your story into |a webpage','20px',33,509,'24.5','350', 'left']
+	bannerData.headline4_5 = ['<#505050>+ more','20px',33,568,'24.5','350', 'left']
+
+	bannerData.CTA = ['<#FFFFFF>BUY NOW','14px',7,-1,'50','300', 'center']
+	
+	bannerData.CTAarrowVisible = [true, 10,-0.5]
