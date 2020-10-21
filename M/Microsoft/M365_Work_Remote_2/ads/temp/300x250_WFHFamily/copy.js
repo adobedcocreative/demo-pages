1f7b9b8cf97e3d88bc6ed340d39b7397025131a2
Neeraj Deshmukh <ndeshmuk@adobe.com>
2020-10-21 09:19:46 +0530

    Altice Update

---
+++ M/Microsoft/M365_Work_Remote_2/ads/temp/300x250_WFHFamily/copy.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+	
+	
+	bannerData.headline1 = ["<#505050>Where","24px",150,109 ,"26","350", "center"];
+	bannerData.headline2 = ["<#505050>and","24px",150,109 ,"26","350", "center"];
+	bannerData.headline3 = ["<#505050>how we","24px",150,109 ,"26","350", "center"];
+	bannerData.headline4 = ["<#505050>work","24px",150,109 ,"26","350", "center"];
+    bannerData.headline5 = ["<#505050>has changed","24px",150,109 ,"26","350", "center"];
+
+    bannerData.headline6 = ["<#505050>Get","24px",150,109 ,"26","350", "center"];
+	bannerData.headline7 = ["<#505050>the","24px",150,109 ,"26","350", "center"];
+	bannerData.headline8 = ["<#505050>tools","24px",150,109 ,"26","350", "center"];
+	bannerData.headline9 = ["<#505050>you","24px",150,109 ,"26","350", "center"];
+    bannerData.headline10 = ["<#505050>need to","24px",150,109 ,"26","350", "center"];
+    bannerData.headline11 = ["<#505050>thrive","24px",150,109 ,"26","350", "center"];
+
+
+
+	bannerData.headline20 = ["<#505050>From conference |room to kitchen |table","18px",16,42 ,"20","350", "left"];
+	bannerData.headline30 = ["<#505050>Empower productivity |anywhere with |Microsoft 365","11.5px",16,111,"14","350", "left"];
+
+	bannerData.CTA = ["<#FFFFFF>BUY NOW","12px",0,0,"16","300", "left"];
+	
+	bannerData.CTAarrowVisible = [true, 0,0]
---
+++ M/Microsoft/M365_Work_Remote_2/ads/temp/728x90_YPHeadphones/copy.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+
+	bannerData.headline1 = ["<#505050>From conference room|to kitchen table","18.3px",393,7,"20","350", "left"];
+	bannerData.headline2 = ["<#505050>Empower productivity anywhere|with Microsoft 365","12.2px",394,53,"14","350", "left"];
+
+	bannerData.CTA = ["<#FFFFFF>BUY NOW","12px",0,0,"50","300", "center"];
+	
+	bannerData.CTAarrowVisible = [true, 0,0]
