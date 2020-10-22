ceb0d31cf5e969543d8fbd2dc08adb32bd7fee30
Neeraj Deshmukh <ndeshmuk@adobe.com>
2020-10-22 15:28:59 +0530

    Microsoft Update

--- M/Microsoft/M365_Work_Remote_2/js/content.js
+++ M/Microsoft/M365_Work_Remote_2/js/content.js
@@ -4,7 +4,7 @@ var adData = [];
 var loadTemplateFlag1 = false;
 var getFeed1 = function(){
   var xmlhttp = new XMLHttpRequest();
-  var url = "https://spreadsheets.google.com/feeds/list/1-APSBKSq6VqhZw9rgK6E5gYJ5QxzLi8qapppcr9OAqs/1/public/values?alt=json";
+  var url = "https://spreadsheets.google.com/feeds/list/19Yd91pBaOeU02YsN96BwaAYIfdyHH6zhT3pxnEbQLvg/1/public/values?alt=json";
 
   xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
