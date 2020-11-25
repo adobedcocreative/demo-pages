27b1b0a7c5765d5ed40cbcd8e7ff3b34279c4026
Neeraj Deshmukh <ndeshmuk@adobe.com>
2020-11-25 19:22:01 +0530

    Microsoft Update

---
+++ M/Microsoft/MicrosoftAzure/Microsoft_Azure_Base_Template_V4/ads/160x600/Migration/copy.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+	
+	
+	
+	bannerData.headline1 = ["<#000000>Migrate your|on-premises|workloads to|the cloud with|confidence","21px",16, 75,"25","350", "left"];
+
+	bannerData.CTA = ["<#FFFFFF>Try Azure free","13.5px",-5,0,"50","300", "center"];
+	
+	bannerData.CTAarrowVisible = [true, 0,0]
---
+++ M/Microsoft/MicrosoftAzure/Microsoft_Azure_Base_Template_V4/ads/300x600/Migration/copy.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+	
+	
+	
+	bannerData.headline1 = ["<#000000>Migrate your on-premises|workloads to the cloud|with confidence","22.5px",15, 75,"27","350", "left"];
+
+	bannerData.CTA = ["<#ffffff>Try Azure free","15px",-13,0,"50","300", "center"];
+	
+	bannerData.CTAarrowVisible = [true, 0,0]
