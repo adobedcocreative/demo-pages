fcbd011fed6a7549784c3b410dc57f642f91b943
Neeraj Deshmukh <ndeshmuk@adobe.com>
2020-12-02 18:33:03 +0530

    Microsoft Update

---
+++ M/Microsoft/2021/M365_New_Year_Mobile_v1/ads/temp/160x600_ProductivitySuite/copy.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+	
+	
+	
+	bannerData.headline1 = ["<#505050>Start fresh |with |Microsoft 365","20.5px",16,59 ,"22","350", "left"];
+	bannerData.headline2 = ["<#505050>Premium Office |apps, cloud storage, |and more","12px",16,131 ,"14","350", "left"];
+
+	bannerData.CTA = ["<#FFFFFF>LEARN MORE","14px",4,-1,"19","300", "center"];
+	
+	bannerData.CTAarrowVisible = [true, 6,0]
---
+++ M/Microsoft/2021/M365_New_Year_Mobile_v1/ads/temp/300x600_ProductivitySuite/copy.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+	
+	
+	
+	bannerData.headline1 = ["<#505050>Start fresh with |Microsoft 365","22.5px",31,74 ,"24","350", "left"];
+	bannerData.headline2 = ["<#505050>Premium Office apps, cloud |storage, and more","14px",31,129 ,"15","350", "left"];
+
+	bannerData.CTA = ["<#FFFFFF>LEARN MORE","13px",4,-1,"19","300", "center"];
+	
+	bannerData.CTAarrowVisible = [true, 5,0]
