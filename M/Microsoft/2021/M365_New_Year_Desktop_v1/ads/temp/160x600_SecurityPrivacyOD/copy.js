caad1b9cb21499360415ae92b0164afe81710b65
Neeraj Deshmukh <ndeshmuk@adobe.com>
2020-12-14 15:36:41 +0530

    Microsoft Update

---
+++ M/Microsoft/2021/M365_New_Year_Desktop_v1/ads/temp/160x600_SecurityPrivacyOD/copy.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+	
+	bannerData.headline1 = ["<#FFFFFF>Start fresh with|Microsoft 365","19px",80, 126,"20","3 50", "center"];
+	bannerData.headline2 = ["<#FFFFFF>Keep valuable files |organized and secured in |your Personal Vault","11px",80, 174,"13","3 50", "center"];
+	bannerData.CTA = ["<#0078D4>LEARN MORE","12px",-1,0,"50","300", "center"];
+	
+	bannerData.CTAarrowVisible = [true, 0,0]
