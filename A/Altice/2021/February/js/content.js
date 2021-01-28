1b0a1dd4ed9d83d10c464bc078d5cc1315f15e07
Neeraj Deshmukh <ndeshmuk@adobe.com>
2021-01-28 10:28:12 +0530

    Altice Update

--- A/Altice/2021/February/js/content.js
+++ A/Altice/2021/February/js/content.js
@@ -4,7 +4,7 @@ var adData = [];
 var loadTemplateFlag1 = false;
 var getFeed1 = function(){
   var xmlhttp = new XMLHttpRequest();
-  var url = "https://spreadsheets.google.com/feeds/list/1AOqeyDj0s6f3KZ9s-nxJGNWBCOxK9Gh4qZUFkpCci_0/3/public/values?alt=json";
+  var url = "https://spreadsheets.google.com/feeds/list/1AOqeyDj0s6f3KZ9s-nxJGNWBCOxK9Gh4qZUFkpCci_0/2/public/values?alt=json";
 
   xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
