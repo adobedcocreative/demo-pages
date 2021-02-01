02c68e67bb4074b1be8f6a7b99b84c82d1ff76f5
Neeraj Deshmukh <ndeshmuk@adobe.com>
2021-02-01 21:35:11 +0530

    Microsoft Update

--- M/Microsoft/2021/M365_FY21Q3_Desktop/ads/160x600/CarouselDesktop/copy.js
+++ M/Microsoft/2021/M365_FY21Q3_Desktop/ads/160x600/CarouselDesktop/copy.js
@@ -4,10 +4,10 @@
 	// when this occurs you can use line height to adjust line spacing.
 	// lineWidth is no longer needed.
 	var bannerData = new Object()
-	
+
 	bannerData.headline1 = ['<#505050>Experience the|benefits of|Microsoft 365','18px',15,43,'20','350', 'left']
 	bannerData.headline2 = ['<#505050>Do more with |premium features |and OneDrive |cloud storage','12.3px',16,110,'14','350','left']
-	
+
 	bannerData.enlarged_1_1 = ['<#505050>Protect|photos across|devices','18px',15,43,'20','350', 'left']
 	bannerData.enlarged_1_2 = ['<#505050>Quickly find what |matters with 1 TB |of OneDrive storage','12.3px',16,110,'14','350','left']
 
@@ -21,5 +21,5 @@
 	bannerData.enlarged_4_2 = ['<#505050>Create with ease |using smart features','12.3px',16,133,'14','350','left']
 
 	bannerData.CTA = ['<#FFFFFF>BUY NOW','13px',0,0,'50','300', 'center']
-	
+
 	bannerData.CTAarrowVisible = [true, 0,0]
