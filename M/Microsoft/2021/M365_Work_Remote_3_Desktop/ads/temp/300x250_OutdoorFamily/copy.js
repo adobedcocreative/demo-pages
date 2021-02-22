5da5150b3e6653e397820351b1e9ea6f9fe753be
Neeraj Deshmukh <ndeshmuk@adobe.com>
2021-02-22 23:21:37 +0530

    Microsoft Update

---
+++ M/Microsoft/2021/M365_Work_Remote_3_Desktop/ads/temp/300x250_OutdoorFamily/copy.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+	
+	
+	bannerData.headline1 = ['<#505050>Where','24px',150,109 ,'26','350', 'center']
+	bannerData.headline2 = ['<#505050>and','24px',150,109 ,'26','350', 'center']
+	bannerData.headline3 = ['<#505050>how','24px',108,109 ,'26','350', 'left']
+    bannerData.headline3a = ['<#505050>we','24px',161,109 ,'26','350', 'left']
+	bannerData.headline4 = ['<#505050>work','24px',150,109 ,'26','350', 'center']
+    bannerData.headline5 = ['<#505050>has','24px',82,109 ,'26','350', 'left']
+    bannerData.headline5a = ['<#505050>changed','24px',126,109 ,'26','350', 'left']
+
+    bannerData.headline6 = ['<#505050>Get','24px',150,109 ,'26','350', 'center']
+	bannerData.headline7 = ['<#505050>the','24px',150,109 ,'26','350', 'center']
+	bannerData.headline8 = ['<#505050>tools','24px',150,109 ,'26','350', 'center']
+	bannerData.headline9 = ['<#505050>you','24px',150,109 ,'26','350', 'center']
+    bannerData.headline10 = ['<#505050>need','24px',108,109 ,'26','350', 'left']
+    bannerData.headline10a = ['<#505050>to','24px',168,109 ,'26','350', 'left']
+    bannerData.headline11 = ['<#505050>thrive','24px',150,109 ,'26','350', 'center']
+
+
+
+	bannerData.headline20 = ['<#505050>Stay productive|while you juggle|it all','18px',16,43 ,'20','350', 'left']
+	bannerData.headline30 = ['<#505050>Get more done in less|time with the powerful|productivity apps of|Microsoft 365','12px',16,111,'14','350', 'left']
+
+	bannerData.CTA = ['<#FFFFFF>BUY NOW','12px',0,0,'16','300', 'left']
+	
+	bannerData.CTAarrowVisible = [true, 0,0]
---
+++ M/Microsoft/2021/M365_Work_Remote_3_Desktop/ads/temp/300x600_INTCarousel/copy.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+	
+	bannerData.headline1 = ['<#505050>Work where you're |comfortable','20.4px',32, 60,'22','3 50', 'left']
+	bannerData.headline2 = ['<#505050>Change up your |remote routine','20.4px',32, 60,'22','3 50', 'left']
+	bannerData.headline3 = ['<#505050>Bring out your best work','20.4px',32, 60,'22','3 50', 'left']
+	bannerData.headline4 = ['<#505050>Stay productive while |you juggle it all','20.4px',32, 60,'22','3 50', 'left']
+
+	bannerData.subheadline1 = ['<#505050>Accomplish more, faster, with premium |Office apps in Microsoft 365','14.2px',32, 115,'16','3 50', 'left']
+
+	bannerData.CTA = ['<#FFFFFF>BUY NOW','13.9px',16,2,'50','300', 'left']
+	
+	bannerData.CTAarrowVisible = [true, 0,0]
