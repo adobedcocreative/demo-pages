f2f3ab8761822e3d22c88a56d198843744d48d81
Neeraj Deshmukh <ndeshmuk@adobe.com>
2021-03-04 10:45:50 +0530

    Microsoft Update

---
+++ M/Microsoft/MicrosoftAzure/MicrosoftAzure_Free Account_1/js/content.js
@@ -0,0 +1,100 @@
+var feedTemplate1 = [] = [];
+var feedContent = [], feedData = {}, getData;
+var adData = [];
+var loadTemplateFlag1 = false;
+var getFeed1 = function(){
+  var xmlhttp = new XMLHttpRequest();
+  var url = "https://spreadsheets.google.com/feeds/list/1R3EZiVM_YN5QPKA8XqE0gANPl6eVW8uZAy2tqSL6E-s/6/public/values?alt=json";
+
+  https://docs.google.com/spreadsheets/d/1R3EZiVM_YN5QPKA8XqE0gANPl6eVW8uZAy2tqSL6E-s/edit#gid=348621748
+
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          var JSONData = JSON.parse(this.responseText);
+          JSONData.feed.entry.map(function(data){
+            feedTemplate1.push({
+              "Ad Size": data['gsx$adsize']['$t'],
+              "ctaText": data['gsx$ctatext']['$t'],
+              "Country": data['gsx$country']['$t'],
+              "Language": data['gsx$language']['$t'],
+              "Smart Names": data['gsx$smartnames']['$t'],
+              "backgroundImage": data['gsx$backgroundimage']['$t'],
+              "headline": data['gsx$headline']['$t'],
+              "subHeadline": data['gsx$subheadline']['$t'],
+            });
+          });
+          loadTemplateFlag1 = true;
+          loadData();
+      }
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
+getFeed1();
+
+var loadData = function(){
+  if(loadTemplateFlag1) {
+    feedContent = [
+      {
+        name: 'Simplified',
+        feed: feedTemplate1,
+      },
+    ]
+    feedContent.map(function(feed){
+    	var feedContent1 = [];
+    	feed.feed.map(function(data){
+        data.template = feed.name;
+    		if(data.Country.indexOf('/') != -1){
+                data.Country.split('/').map(function(country){
+                    var obj = {};
+                    for(var i in data) { obj[i] = data[i] }
+                    obj['Country'] = country;
+                    feedContent1.push(obj);
+                });
+            } else {
+                feedContent1.push(data);
+            }
+    	});
+    	feed.data = feedContent1;
+
+      feed.data.map(function(data){
+        if(!(data.Country in feedData)) { feedData[data.Country] = []; }
+        feedData[data.Country].push(data);
+      });
+    });
+    for(var i in feedData) {
+      var obj = {};
+      obj.name = i;
+      obj.data = [];
+      var smartNames = [];
+      feedData[i].map(function(data){smartNames.push(data['Smart Names']);});
+      smartNames = smartNames.filter(function(value, index, self){ return self.indexOf(value) === index; })
+      smartNames.map(function(smartName){
+        var smartObject = {};
+        smartObject.name = smartName;
+        smartObject.data = feedData[i].filter(function(data){ return data['Smart Names'] == smartName });
+        obj.data.push(smartObject);
+      });
+      adData.push(obj);
+    }
+    getData = function(queryString) {
+      var searchData;
+      if(queryString) {
+        if(queryString.indexOf('|') != -1) {
+          var country = queryString.split('|')[0];
+          var smartName = queryString.split('|')[1];
+          var data = adData.find(function(data){ return data.name == country })
+          if(data) {
+            data = data.data.find(function(data){ return data.name == smartName })
+            if(data) { searchData = data }
+          }
+        } else {
+          var data = adData.find(function(data){ return data.name == queryString});
+          if(data) { searchData = data.data[0] } else { searchData = adData[0].data[0] }
+        }
+      }
+      return searchData ? searchData : adData[0].data[0];
+    }
+    loadPage();
+  }
+}
