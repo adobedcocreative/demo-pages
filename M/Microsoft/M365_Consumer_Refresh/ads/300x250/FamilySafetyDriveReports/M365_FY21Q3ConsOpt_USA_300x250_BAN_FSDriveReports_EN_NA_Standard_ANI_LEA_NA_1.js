e64b62989f7ccfd31f4469635e1a05226a16611e
Neeraj Deshmukh <ndeshmuk@adobe.com>
2021-03-16 15:36:49 +0530

    Microsoft Update

--- M/Microsoft/M365_Consumer_Refresh/ads/300x250/FamilySafetyDriveReports/M365_FY21Q3ConsOpt_USA_300x250_BAN_FSDriveReports_EN_NA_Standard_ANI_LEA_NA_1.js
+++ M/Microsoft/M365_Consumer_Refresh/ads/300x250/FamilySafetyDriveReports/M365_FY21Q3ConsOpt_USA_300x250_BAN_FSDriveReports_EN_NA_Standard_ANI_LEA_NA_1.js
@@ -788,15 +788,15 @@ if (reversed == null) { reversed = false; }
 		}
 		var mc = exportRoot.mainMC
 		exportRoot.replayAnim = "inProgress"
-		
-		
+
+
 		this.initBanner = function (data) {
-			
+
 			exportRoot.isReplay = false;
-			
+
 			Object.keys = function(obj) {
 				var keys = [];
-		
+
 				for (var i in obj) {
 				  if (obj.hasOwnProperty(i)) {
 					keys.push(i);
@@ -805,7 +805,7 @@ if (reversed == null) { reversed = false; }
 				return keys
 			}
 			var keys = Object.keys(data)
-			
+
 				for (var i in keys) {
 					var id = keys[i].substr(0, 4);
 						if (id == "head") {
@@ -823,8 +823,8 @@ if (reversed == null) { reversed = false; }
 						}
 				}
 		}
-		
-		
+
+
 		this.fillHead = function (txtDetails, aVar) {
 			var text = txtDetails[0]
 			var size = txtDetails[1]
@@ -833,16 +833,16 @@ if (reversed == null) { reversed = false; }
 			var lineSpacing = txtDetails[4]
 			var lineWidth = txtDetails[5]
 			var align = txtDetails[6]
-		
+
 			var aSentenceLine = this.getTheSentences(text, size, xOffset, yOffset, lineSpacing, lineWidth, align)
-		
+
 			for (var i = 0; i < aSentenceLine.length; i++) {
 				var mc = new createjs.MovieClip();
 				mc.y = (i * parseInt(lineSpacing))
 				mc.y += yOffset
 				mc.y += parseInt(size)
 				mc.x += xOffset
-		
+
 				var txtWidth = 0
 				for (var j = 0; j < aSentenceLine[i].length; j++) {
 					var text = new createjs.Text(aSentenceLine[i][j].txt, "normal " + size + " Segoe Pro", aSentenceLine[i][j].color);
@@ -867,16 +867,16 @@ if (reversed == null) { reversed = false; }
 			var lineSpacing = txtDetails[4]
 			var lineWidth = txtDetails[5]
 			var align = txtDetails[6]
-		
+
 			var aSentenceLine = this.getTheSentences(text, size, xOffset, yOffset, lineSpacing, lineWidth, align)
-		
+
 			for (var i = 0; i < aSentenceLine.length; i++) {
 				var mc = new createjs.MovieClip();
 				mc.y = (i * parseInt(lineSpacing))
 				mc.y += yOffset
 				mc.y += parseInt(size)
 				mc.x += xOffset
-		
+
 				var txtWidth = 0
 				for (var j = 0; j < aSentenceLine[i].length; j++) {
 					var text = new createjs.Text(aSentenceLine[i][j].txt, "normal " + size + " Segoe Pro", aSentenceLine[i][j].color);
@@ -893,9 +893,9 @@ if (reversed == null) { reversed = false; }
 				aVar.push(mc)
 			}
 		}
-		
-		
-		
+
+
+
 		this.fillCta = function (txtDetails) {
 			var text = txtDetails[0]
 			var size = txtDetails[1]
@@ -904,17 +904,17 @@ if (reversed == null) { reversed = false; }
 			var lineSpacing = txtDetails[4]
 			var lineWidth = txtDetails[5]
 			var align = txtDetails[6]
-		
-		
+
+
 			var aSentenceLine = this.getTheSentences(text, size, xOffset, yOffset, lineSpacing, lineWidth, align)
-		
+
 			for (var i = 0; i < aSentenceLine.length; i++) {
 				var mc = new createjs.MovieClip();
 				mc.y = (i * parseInt(lineSpacing))
 				mc.y += yOffset
 				mc.y += (parseInt(size) * 0.90)
 				mc.x += xOffset
-		
+
 				var txtWidth = 0
 				for (var j = 0; j < aSentenceLine[i].length; j++) {
 					var text = new createjs.Text(aSentenceLine[i][j].txt, "normal " + size + " Segoe Pro", aSentenceLine[i][j].color);
@@ -931,7 +931,7 @@ if (reversed == null) { reversed = false; }
 				this.mainMC.txtCta.addChild(mc);
 			}
 		}
-		
+
 		this.getTheSentences = function (text, size, xOffset, yOffset, lineSpacing, lineWidth, align) {
 			var sentences = new Array()
 			var aSentenceLine = new Array()
@@ -939,16 +939,16 @@ if (reversed == null) { reversed = false; }
 			sentences = aStr.split("|");
 			var lastColor = "#000000"
 			// Figure out the setence lines
-		
+
 			for (var i = 0; i < sentences.length; i++) {
 				var aS = sentences[i].substr(0);
 				var aSplit = new Array()
 				aSplit = aS.split("<");
 				aSplit = aSplit.filter(Boolean)
 				var wholeSentence = new Array()
-		
+
 				for (var j = 0; j < aSplit.length; j++) {
-		
+
 					var checkColor = aSplit[j].indexOf("#")
 					var color = (checkColor == -1) ? lastColor : aSplit[j].substr(0, 7);
 					lastColor = color
@@ -963,61 +963,61 @@ if (reversed == null) { reversed = false; }
 			}
 			return aSentenceLine
 		}
-		
+
 		var mc = exportRoot.mainMC;
 		var screen = mc.screen;
-		
+
 		mc.cta.alpha=0
 		mc.replay_btn.alpha=0
 		mc.screen.alpha=0
-		
+
 		this.runBanner = function() {
-			
+
 				mc.cta.alpha=1
 				mc.replay_btn.alpha=1
 				mc.screen.alpha=1
-				
+
 				exportRoot.tlText = gsap.timeline();
-		
+
 				exportRoot.tlText.from(exportRoot.headline1, 0.05, {alpha: 0, ease:Power4.easeOut});
 				exportRoot.tlText.from(exportRoot.headline2, 0.05, {alpha: 0, ease:Power4.easeOut}, ">+.1");
-		
+
 				exportRoot.tlText.to([exportRoot.headline1, exportRoot.headline2], 0.05, {alpha: 0, ease:Power4.easeOut}, "+=0.5");
-				
+
 				exportRoot.tlText.from(exportRoot.headline3, 0.05, {alpha: 0, ease:Power4.easeOut},">");
 				exportRoot.tlText.from(exportRoot.headline4, 0.05, {alpha: 0, ease:Power4.easeOut}, ">+.1");
-		
+
 				exportRoot.tlText.to([exportRoot.headline3, exportRoot.headline4], 0.05, {alpha: 0, ease:Power4.easeOut}, ">+.4");
-		
+
 				exportRoot.tlText.from(exportRoot.headline5, 0.05, {alpha: 0, ease:Power4.easeOut},">");
 				exportRoot.tlText.from(exportRoot.headline6, 0.05, {alpha: 0, ease:Power4.easeOut}, ">+.1");
-		
+
 				exportRoot.tlText.to([exportRoot.headline5, exportRoot.headline6], 0.05, {alpha: 0, ease:Power4.easeOut}, ">+.4");
-		
+
 				exportRoot.tlText.from(exportRoot.headline7, 0.05, {alpha: 0, ease:Power4.easeOut}, ">");
 				exportRoot.tlText.to(exportRoot.headline7, 0.05, {alpha: 0, ease:Power4.easeOut}, ">+.4");
-				
+
 				exportRoot.tlText.from(exportRoot.headline8, 0.05, {alpha: 0, ease:Power4.easeOut},">");
 				exportRoot.tlText.from(exportRoot.headline9, 0.05, {alpha: 0, ease:Power4.easeOut}, ">+.1");
-		
+
 				exportRoot.tlText.to([exportRoot.headline8, exportRoot.headline9], 0.05, {alpha: 0, ease:Power4.easeOut, onStart:function(){exportRoot.videoState="playing"; vid.play();}}, ">+.4");
-		
+
 				//final frame
-				
+
 				this.tlText.from(mc.screen, 1, { alpha:0, x: "+=150", ease:Power4.easeOut}, "+=6");
-		
+
 				exportRoot.tlText.from(exportRoot.headline10, 0.8, {x: "-=150", alpha: 0, ease:Power4.easeOut, stagger:0.1}, "-=0.8");
-		
+
 				exportRoot.tlText.from(exportRoot.headline11, 0.8, {x: "-=150", alpha: 0, ease:Power4.easeOut, stagger:0.1}, "-=0.6");
-				
+
 				exportRoot.tlText.from(exportRoot.headline12, 0.8, {x: "-=150", alpha: 0, ease:Power4.easeOut, stagger:0.1}, "-=0.6");
-						
-				this.tlText.from(mc.txtCta, 0.6, { x: "-=150", ease:Power4.easeOut, onComplete:function(){exportRoot.videoState="reset"}}, "-=0.6");
-				this.tlText.from(mc.cta, 0.6, { x: "-=150", ease:Power4.easeOut}, "-=0.6");
+
+				this.tlText.from(mc.txtCta, 0.6, { x: "-=200", ease:Power4.easeOut, onComplete:function(){exportRoot.videoState="reset"}}, "-=0.6");
+				this.tlText.from(mc.cta, 0.6, { x: "-=200", ease:Power4.easeOut}, "-=0.6");
 				this.tlText.from(mc.replay_btn, 1, { alpha: 0, onStart:function(){exportRoot.isReplay = true;}}, "-=0.4");
-				
+
 				exportRoot.tlText.pause();
-				
+
 				mc.logo_intro.gotoAndPlay(1);
 				setTimeout(function(){ vid.style.opacity = 1; }, 10);
 		}
@@ -1103,41 +1103,41 @@ an.getComposition = function(id) {
 }
 
 
-an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers) {		
-	var lastW, lastH, lastS=1;		
-	window.addEventListener('resize', resizeCanvas);		
-	resizeCanvas();		
-	function resizeCanvas() {			
-		var w = lib.properties.width, h = lib.properties.height;			
-		var iw = window.innerWidth, ih=window.innerHeight;			
-		var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;			
-		if(isResp) {                
-			if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {                    
-				sRatio = lastS;                
-			}				
-			else if(!isScale) {					
-				if(iw<w || ih<h)						
-					sRatio = Math.min(xRatio, yRatio);				
-			}				
-			else if(scaleType==1) {					
-				sRatio = Math.min(xRatio, yRatio);				
-			}				
-			else if(scaleType==2) {					
-				sRatio = Math.max(xRatio, yRatio);				
-			}			
+an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers) {
+	var lastW, lastH, lastS=1;
+	window.addEventListener('resize', resizeCanvas);
+	resizeCanvas();
+	function resizeCanvas() {
+		var w = lib.properties.width, h = lib.properties.height;
+		var iw = window.innerWidth, ih=window.innerHeight;
+		var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;
+		if(isResp) {
+			if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {
+				sRatio = lastS;
+			}
+			else if(!isScale) {
+				if(iw<w || ih<h)
+					sRatio = Math.min(xRatio, yRatio);
+			}
+			else if(scaleType==1) {
+				sRatio = Math.min(xRatio, yRatio);
+			}
+			else if(scaleType==2) {
+				sRatio = Math.max(xRatio, yRatio);
+			}
 		}
-		domContainers[0].width = w * pRatio * sRatio;			
+		domContainers[0].width = w * pRatio * sRatio;
 		domContainers[0].height = h * pRatio * sRatio;
-		domContainers.forEach(function(container) {				
-			container.style.width = w * sRatio + 'px';				
-			container.style.height = h * sRatio + 'px';			
+		domContainers.forEach(function(container) {
+			container.style.width = w * sRatio + 'px';
+			container.style.height = h * sRatio + 'px';
 		});
-		stage.scaleX = pRatio*sRatio;			
+		stage.scaleX = pRatio*sRatio;
 		stage.scaleY = pRatio*sRatio;
-		lastW = iw; lastH = ih; lastS = sRatio;            
-		stage.tickOnUpdate = false;            
-		stage.update();            
-		stage.tickOnUpdate = true;		
+		lastW = iw; lastH = ih; lastS = sRatio;
+		stage.tickOnUpdate = false;
+		stage.update();
+		stage.tickOnUpdate = true;
 	}
 }
 an.handleSoundStreamOnTick = function(event) {
