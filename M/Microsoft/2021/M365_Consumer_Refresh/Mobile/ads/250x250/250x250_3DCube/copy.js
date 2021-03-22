91707d2b514492579618550bc463b4705b1fa817
Neeraj Deshmukh <ndeshmuk@adobe.com>
2021-03-23 00:02:51 +0530

    Microsoft Update

---
+++ M/Microsoft/2021/M365_Consumer_Refresh/Mobile/ads/250x250/250x250_3DCube/copy.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+
+	//bannerData.headline1 = ['<#000000> ','22.5px',31,71,'28','350', 'left']
+
+	bannerData.headline1 = ['<#505050>Bring out |your best, |everywhere','37px',40,104,'39','350', 'left']
+	bannerData.subhead1 = ['<#505050>Premium apps |and OneDrive |cloud storage','23.5px',40,246,'26','350', 'left']
+
+	bannerData.headline2 = ['<#505050>Present from |the palm of |your hand','37px',40,104,'39','350', 'left']
+	bannerData.subhead2 = ['<#505050>Design quickly |with smart tools','23.5px',40,246,'26','350', 'left']
+
+	bannerData.headline3 = ['<#505050>Write with style |on mobile','37px',40,104,'39','350', 'left']
+	bannerData.subhead3 = ['<#505050>With smart |assistance features','23.5px',40,205,'26','350', 'left']
+
+	bannerData.headline4 = ['<#505050>Budget on |the fly','37px',40,104,'39','350', 'left']
+	bannerData.subhead4 = ['<#505050>Make decisions |with smart features','23.5px',40,205,'26','350', 'left']
+
+	bannerData.CTA = ['<#FFFFFF>Learn more','10px',-2,0,'50','300', 'center']
+	
+	bannerData.CTAarrowVisible = [true, 0,0.5]
---
+++ M/Microsoft/2021/M365_Consumer_Refresh/Mobile/ads/temp/300x250_3DCube/copy.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+
+	//bannerData.headline1 = ['<#000000> ','22.5px',31,71,'28','350', 'left']
+
+	bannerData.headline1 = ['<#505050>Bring out |your best, |everywhere','36px',31,10,'39','350', 'left']
+	bannerData.subhead1 = ['<#505050>Premium apps and |OneDrive cloud |storage','22px',31,147,'24','350', 'left']
+
+	bannerData.headline2 = ['<#505050>Present from the palm |of your hand','36px',31,10,'39','350', 'left']
+	bannerData.subhead2 = ['<#505050>Design quickly |with smart tools','22px',31,108,'24','350', 'left']
+
+	bannerData.headline3 = ['<#505050>Write with style on mobile','36px',31,10,'39','350', 'left']
+	bannerData.subhead3 = ['<#505050>With smart |assistance features','22px',31,69,'24','350', 'left']
+
+	bannerData.headline4 = ['<#505050>Budget on the fly','36px',31,10,'39','350', 'left']
+	bannerData.subhead4 = ['<#505050>Make decisions with |smart features','22px',31,69,'24','350', 'left']
+
+	bannerData.CTA = ['<#FFFFFF>Learn more','10px',-2,0,'50','300', 'center']
+	
+	bannerData.CTAarrowVisible = [true, 0,0.5]
