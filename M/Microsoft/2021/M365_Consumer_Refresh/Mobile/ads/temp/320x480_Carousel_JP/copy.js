8afe57eae04e6bc4c5a0a81198365532cf61e3c5
Neeraj Deshmukh <ndeshmuk@adobe.com>
2021-04-30 06:46:25 +0530

    Microsoft Update

---
+++ M/Microsoft/2021/M365_Consumer_Refresh/Mobile/ads/temp/320x480_Carousel_JP/copy.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+	
+	bannerData.headline1 = ["<#505050>Intelligence that has |your back","20.4px",32 , 61,"22.5","3 50", "left"];
+	bannerData.headline2 = ["<#505050>Tag. Store. |Share memories.","20.4px",32 , 61,"22.5","3 50", "left"];
+	bannerData.headline3 = ["<#505050>Tell amazing stories","20.4px",32 , 61,"22.5","3 50", "left"];
+	bannerData.headline4 = ["<#505050>Bring out your best style","20.4px",32 , 61,"22.5","3 50", "left"];
+	bannerData.headline5 = ["<#505050>Leave the heavy |lifting to Excel","20.4px",32, 61,"22.5","3 50", "left"];
+	bannerData.headline6 = ["<#505050>Kick out the clutter |with an ad-free inbox","20.4px",32 , 61,"22.5","3 50", "left"];
+
+	bannerData.subheadline1 = ["<#505050>Plus 1 TB of OneDrive cloud storage","14.2px",32, 111,"14","3 50", "left"];
+
+	bannerData.smallPrint1 = ["<#505050>Features apply to customers who have an @outlook.com, @hotmail.com, |@live.com, and @msn.com","8.3px",17, 454,"8","3 50", "left"];
+
+	bannerData.smallPrint2 = ["<#505050>App available free; Microsoft 365 subscription unlocks premium features","8.3px",17, 454,"8","3 50", "left"];
+
+	bannerData.CTA = ["<#FFFFFF>Learn more","12.2px",16,1,"50","300", "left"];
+	
+	bannerData.CTAarrowVisible = [true, 0,0];
