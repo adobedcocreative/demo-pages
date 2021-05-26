d2e4dd30ee9253d2d5a5c099ccb18a1cf816fc36
Rajesh Kumar <rajeshku@adobe.com>
2021-05-26 17:04:24 +0530

    AMO-144077
    Added Template 15

---
+++ E/Epic/EpicPass_Template_15/js/content.js
@@ -0,0 +1,114 @@
+var feedTemplate1 = [];
+var feedContent = [], feedData = {}, getData;
+var adData = [];
+var loadTemplateFlag1 = false;
+var sheetID = '';
+var getFeed1 = function(){
+  var xmlhttp = new XMLHttpRequest();
+  sheetID = "1uVGXQ0uRQ4j9k3HqF5JzacZhPiNUdMyRCdmhm11f0PA/1";
+  var searchID = location.search.split('?')[1];
+  sheetID = searchID && searchID.length == 46 && searchID.indexOf('/') > 1 ? searchID : sheetID;
+  sheetID = searchID && searchID.length <= 2 && Boolean(parseInt(searchID)) ? sheetID.split('/')[0] + '/' + parseInt(searchID) : sheetID;
+  var url = "https://spreadsheets.google.com/feeds/list/" + sheetID + "/public/values?alt=json";
+
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          var JSONData = JSON.parse(this.responseText);
+          JSONData.feed.entry.map(function(data){
+            // feedTemplate1.push({
+            //   "Ad Size": data['gsx$adsize']['$t'],
+            //   "Language": data['gsx$language']['$t'],
+            //   "Group": data['gsx$group']['$t'],
+            //   "Smart Names": data['gsx$smartnames']['$t'],
+            //   "Visibility": data['gsx$visibility']['$t'],
+            // });
+            feedTemplate1.push({
+              "Ad Size": data['gsx$adsize']['$t'],
+              "Frame_1_LogoImage": data['gsx$frame1logoimage']['$t'],
+              "Frame_2_BgImage": data['gsx$frame2bgimage']['$t'],
+              "Country": data['gsx$country']['$t'],
+              "Smart Names": data['gsx$smartnames']['$t'],
+              "Frame_2_LogoImage": data['gsx$frame2logoimage']['$t'],
+              "Frame_3_BgImage": data['gsx$frame3bgimage']['$t'],
+              "Frame_3_LogoImage": data['gsx$frame3logoimage']['$t'],
+              "Frame_2_Text": data['gsx$frame2text']['$t'],
+              "Frame_3_Text": data['gsx$frame3text']['$t'],
+              "Frame_4_Text": data['gsx$frame4text']['$t'],
+              "ctaText": data['gsx$ctatext']['$t'],
+              "ctaBtnColor": data['gsx$ctabtncolor']['$t'],
+              "clickURL": data['gsx$clickurl']['$t'],
+              // "Visibility": data['gsx$visibility']['$t'],
+            });
+          });
+          // if(location.hostname && location.hostname != 'localhost') {
+          //   var tempFeed = [];
+          //   feedTemplate1.forEach(function(data){
+          //     if(!Boolean('Visibility' in data) || ('Visibility' in data && data.Visibility.toLowerCase() == 'true')) {
+          //       tempFeed.push(data);
+          //     }
+          //   });
+          //   feedTemplate1 = tempFeed;
+          // }
+          loadTemplateFlag1 = true;
+          loadData();
+      }
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
+getFeed1();
+var loadData = function(){
+  if(loadTemplateFlag1) {
+    feedContent = [];
+    feedTemplate1.forEach(function(feed){
+      var obj = {};
+      for(var i in feed) { obj[i] = feed[i]; }
+      obj['ads'] = feed['Ad Size'].split(',');
+      feedContent.push(obj);
+    });
+    feedContent.map(function(data){
+      if(!(data.Country in feedData)) { feedData[data.Country] = []; }
+      feedData[data.Country].push(data);
+    });
+    for(var i in feedData) {
+      var obj = {};
+      obj.name = i;
+      obj.data = [];
+      var smartNames = [];
+      feedData[i].map(function(data){smartNames.push(data['Smart Names']);});
+      smartNames = smartNames.filter(function(value, index, self){ return self.indexOf(value) === index; })
+      smartNames.map(function(smartName){
+        var smartObject = {};
+        smartObject.name = smartName;
+        smartObject.data = feedData[i].filter(function(data){ return data['Smart Names'] == smartName });
+        obj.data.push(smartObject);
+      });
+      adData.push(obj);
+    }
+    getData = function(queryString) {
+      var searchData, selectedAd;
+      if(queryString) {
+        if(queryString.indexOf('|') != -1) {
+          var country = queryString.split('|')[0];
+          var smartName = queryString.split('|')[1];
+          selectedAd = queryString.split('|')[2];
+          var data = adData.find(function(data){ return data.name == country })
+          if(data) {
+            data = data.data.find(function(data){ return data.name == smartName })
+            if(data) { searchData = data }
+          }
+        } else {
+          var data = adData.find(function(data){ return data.name == queryString});
+          if(data) { searchData = data.data[0] } else { searchData = adData[0].data[0] }
+        }
+      }
+      var selectedData = searchData ? searchData : adData[0].data[0];
+      selectedAd = selectedAd ? selectedAd : '300x250';
+      var obj = {};
+      for(var i in selectedData) { obj[i] = selectedData[i]; }
+      obj.selectedAd = obj.data.find(function(ad){ return ad['Ad Size'] == selectedAd }) ? selectedAd : obj.data[0].ads[0];
+      return obj;
+    }
+    loadPage();
+  }
+}
