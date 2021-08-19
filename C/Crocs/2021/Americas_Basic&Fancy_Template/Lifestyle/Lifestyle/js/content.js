bb8a4af32d0cc7d7e8c66fc4c4228785a2483ba5
Neeraj Deshmukh <adobedcocreative@gmail.com>
2021-08-19 08:28:55 +0530

    Crocs Spreadsheet API Update

--- C/Crocs/2021/Americas_Basic&Fancy_Template/Lifestyle/Lifestyle/js/content.js
+++ C/Crocs/2021/Americas_Basic&Fancy_Template/Lifestyle/Lifestyle/js/content.js
@@ -2,36 +2,51 @@ var celebritiesFeedContent = [];
 var celebritiesFeeds = {}, celebritiesFeedData = [];
 var getCelebrityData;
 var celebritiesLoadFlag = false;
+const tsvTojson = (data) => {
+  const rowsData = data.split('\r\n').map(row => row.split('\t'));
+  const headers = rowsData[0], rows = rowsData.slice(1);
+  data = [];
+  rows.every(row => {
+    if(row.every(cell => cell === '')) return false; //isEmptyRow
+    let obj = {};
+    row.forEach((cell, i) => obj[headers[i]] = cell);
+    data.push(obj);
+    return true;
+  });
+  return data;
+}
 var getCelebrityFeed = function(){
   var xmlhttp = new XMLHttpRequest();
   var url = "https://spreadsheets.google.com/feeds/list/13VLAZFDR5Z0_b7PGLCG7cP_HZWrv_zmzDBUNhENRYww/1/public/values?alt=json";
+  var url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT4Bo9BP6FqXUJVoiPzX9-wXaYO77boilvw8NLMsZDcLiUzKca2tRonpbZAHk6p62mXjRSUKN2UbOo_/pub?output=tsv";
 
   xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-          var JSONData = JSON.parse(this.responseText);
-          JSONData.feed.entry.map(function(data){
-            celebritiesFeedContent.push({
-              "LANGUAGE": data['gsx$language']['$t'],
-              "MODEL": data['gsx$model']['$t'],
-              "AD_SIZE": data['gsx$adsize']['$t'],
-              "smartName": data['gsx$smartname']['$t'],
-              "Label": data['gsx$label']['$t'],
-              "styleProperties": data['gsx$styleproperties']['$t'],
-              "ctaText": data['gsx$ctatext']['$t'],
-              "backgroundImage": data['gsx$backgroundimage']['$t'],
-              "frameImage11": data['gsx$frameimage11']['$t'],
-              "frameImage12": data['gsx$frameimage12']['$t'],
-              "frameImage21": data['gsx$frameimage21']['$t'],
-              "frameImage22": data['gsx$frameimage22']['$t'],
-              "frameImage31": data['gsx$frameimage31']['$t'],
-              "frameImage32": data['gsx$frameimage32']['$t'],
-              "frameImage41": data['gsx$frameimage41']['$t'],
-              "frameImage42": data['gsx$frameimage42']['$t'],
-              "frameImage43": data['gsx$frameimage43']['$t'],
-              "headlineText": data['gsx$headlinetext']['$t'],
-              "clickUrl": data['gsx$url']['$t'],
-            });
-          });
+          // var JSONData = JSON.parse(this.responseText);
+          // JSONData.feed.entry.map(function(data){
+          //   celebritiesFeedContent.push({
+          //     "LANGUAGE": data['gsx$language']['$t'],
+          //     "MODEL": data['gsx$model']['$t'],
+          //     "AD_SIZE": data['gsx$adsize']['$t'],
+          //     "smartName": data['gsx$smartname']['$t'],
+          //     "Label": data['gsx$label']['$t'],
+          //     "styleProperties": data['gsx$styleproperties']['$t'],
+          //     "ctaText": data['gsx$ctatext']['$t'],
+          //     "backgroundImage": data['gsx$backgroundimage']['$t'],
+          //     "frameImage11": data['gsx$frameimage11']['$t'],
+          //     "frameImage12": data['gsx$frameimage12']['$t'],
+          //     "frameImage21": data['gsx$frameimage21']['$t'],
+          //     "frameImage22": data['gsx$frameimage22']['$t'],
+          //     "frameImage31": data['gsx$frameimage31']['$t'],
+          //     "frameImage32": data['gsx$frameimage32']['$t'],
+          //     "frameImage41": data['gsx$frameimage41']['$t'],
+          //     "frameImage42": data['gsx$frameimage42']['$t'],
+          //     "frameImage43": data['gsx$frameimage43']['$t'],
+          //     "headlineText": data['gsx$headlinetext']['$t'],
+          //     "clickUrl": data['gsx$url']['$t'],
+          //   });
+          // });
+          celebritiesFeedContent = tsvTojson(this.responseText);
           celebritiesLoadFlag = true;
           loadData();
           // console.log(celebritiesFeedContent);
--- C/Crocs/2021/Americas_Basic&Fancy_Template/Product/Lifestyle/js/content.js
+++ C/Crocs/2021/Americas_Basic&Fancy_Template/Product/Lifestyle/js/content.js
@@ -2,30 +2,45 @@ var celebritiesFeedContent = [];
 var celebritiesFeeds = {}, celebritiesFeedData = [];
 var getCelebrityData;
 var celebritiesLoadFlag = false;
+const tsvTojson = (data) => {
+  const rowsData = data.split('\r\n').map(row => row.split('\t'));
+  const headers = rowsData[0], rows = rowsData.slice(1);
+  data = [];
+  rows.every(row => {
+    if(row.every(cell => cell === '')) return false; //isEmptyRow
+    let obj = {};
+    row.forEach((cell, i) => obj[headers[i]] = cell);
+    data.push(obj);
+    return true;
+  });
+  return data;
+}
 var getCelebrityFeed = function(){
   var xmlhttp = new XMLHttpRequest();
   var url = "https://spreadsheets.google.com/feeds/list/13VLAZFDR5Z0_b7PGLCG7cP_HZWrv_zmzDBUNhENRYww/2/public/values?alt=json";
+  var url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT4Bo9BP6FqXUJVoiPzX9-wXaYO77boilvw8NLMsZDcLiUzKca2tRonpbZAHk6p62mXjRSUKN2UbOo_/pub?gid=51496328&single=true&output=tsv";
 
   xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-          var JSONData = JSON.parse(this.responseText);
-          JSONData.feed.entry.map(function(data){
-            celebritiesFeedContent.push({
-              "LANGUAGE": data['gsx$language']['$t'],
-              "MODEL": data['gsx$model']['$t'],
-              "AD_SIZE": data['gsx$adsize']['$t'],
-              "smartName": data['gsx$smartname']['$t'],
-              "Label": data['gsx$label']['$t'],
-              "styleProperties": data['gsx$styleproperties']['$t'],
-              "ctaText": data['gsx$ctatext']['$t'],
-              "backgroundColor": data['gsx$backgroundcolor']['$t'],
-              "frameImage1": data['gsx$frameimage1']['$t'],
-              "frameImage2": data['gsx$frameimage2']['$t'],
-              "frameImage3": data['gsx$frameimage3']['$t'],
-              "headlineText": data['gsx$headlinetext']['$t'],
-              "clickUrl": data['gsx$url']['$t'],
-            });
-          });
+          // var JSONData = JSON.parse(this.responseText);
+          // JSONData.feed.entry.map(function(data){
+          //   celebritiesFeedContent.push({
+          //     "LANGUAGE": data['gsx$language']['$t'],
+          //     "MODEL": data['gsx$model']['$t'],
+          //     "AD_SIZE": data['gsx$adsize']['$t'],
+          //     "smartName": data['gsx$smartname']['$t'],
+          //     "Label": data['gsx$label']['$t'],
+          //     "styleProperties": data['gsx$styleproperties']['$t'],
+          //     "ctaText": data['gsx$ctatext']['$t'],
+          //     "backgroundColor": data['gsx$backgroundcolor']['$t'],
+          //     "frameImage1": data['gsx$frameimage1']['$t'],
+          //     "frameImage2": data['gsx$frameimage2']['$t'],
+          //     "frameImage3": data['gsx$frameimage3']['$t'],
+          //     "headlineText": data['gsx$headlinetext']['$t'],
+          //     "clickUrl": data['gsx$url']['$t'],
+          //   });
+          // });
+          celebritiesFeedContent = tsvTojson(this.responseText);
           celebritiesLoadFlag = true;
           loadData();
           // console.log(celebritiesFeedContent);
