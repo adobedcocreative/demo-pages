5612ba27c155e6170b4a285f48c9de45524f5fec
Neeraj Deshmukh <adobedcocreative@gmail.com>
2021-08-19 19:49:52 +0530

    Altice Spreadsheet API Update

--- A/Altice/2021/DCO_Feed/js/content.js
+++ A/Altice/2021/DCO_Feed/js/content.js
@@ -2,56 +2,6 @@ var feedTemplate1 = [];
 var feedContent = [], feedData = {}, getData;
 var adData = [];
 var loadTemplateFlag1 = false;
-const tsvTojson = (data) => {
-  const rowsData = data.split('\r\n').map(row => row.split('\t'));
-  const headers = rowsData[0], rows = rowsData.slice(1);
-  data = [];
-  rows.every(row => {
-    if(row.every(cell => cell === '')) return false; //isEmptyRow
-    let obj = {};
-    row.forEach((cell, i) => obj[headers[i]] = cell);
-    data.push(obj);
-    return true;
-  });
-  return data;
-}
-// var getFeed1 = function(){
-//   var xmlhttp = new XMLHttpRequest();
-//   var sheetID = "1i564DiRU35eN8CpRMm-_dmIUUSHgm6oyAxMWEtfFtpI/1";
-//   var searchID = location.search.split('?')[1];
-//   sheetID = searchID && searchID.length == 46 && searchID.indexOf('/') > 1 ? searchID : sheetID;
-//   sheetID = searchID && searchID.length <= 2 && Boolean(parseInt(searchID)) ? sheetID.split('/')[0] + '/' + parseInt(searchID) : sheetID;
-//   var url = "https://spreadsheets.google.com/feeds/list/" + sheetID + "/public/values?alt=json";
-//   var url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQvw4CgKDU5aOvbFTUyS9HvF94aj5awVd61lVtw7ME1bgeWG-b3sigRbmGCL1R_sM-qraSQ-_5IfWU1/pub?output=tsv";
-
-//   xmlhttp.onreadystatechange = function() {
-//       if (this.readyState == 4 && this.status == 200) {
-//           // var JSONData = JSON.parse(this.responseText);
-//           // JSONData.feed.entry.map(function(data){
-//           //   feedTemplate1.push({
-//           //     "Ad Size": data['gsx$adsize']['$t'],
-//           //     "CTA": data['gsx$cta']['$t'],
-//           //     "Place": data['gsx$place']['$t'],
-//           //     "Variation": data['gsx$variation']['$t'],
-//           //     "Smart Names": data['gsx$smartnames']['$t'],
-//           //     "textField1": data['gsx$textfield1']['$t'],
-//           //     "textField2": data['gsx$textfield2']['$t'],
-//           //     "textField3": data['gsx$textfield3']['$t'],
-//           //     "textField4": data['gsx$textfield4']['$t'],
-//           //     "textField5": data['gsx$textfield5']['$t'],
-//           //     "endFrameImage": data['gsx$endframeimage']['$t'],
-//           //     "URL": data['gsx$url']['$t'],
-//           //     // "Visibility": data['gsx$visibility']['$t'],
-//           //   });
-//           // });
-//           feedTemplate1 = tsvTojson(this.responseText);
-//           loadTemplateFlag1 = true;
-//           loadData();
-//       }
-//   };
-//   xmlhttp.open("GET", url, true);
-//   xmlhttp.send();
-// }
 var getFeed1 = function(){
   var xmlhttp = new XMLHttpRequest();
   const API_KEY = "AIzaSyA9UwsLAgEsktyccelGlG_AV37qUCL-Gqo";
