66ce0db58b08783fea1cacf435c7f477eff14cde
Neeraj Deshmukh <adobedcocreative@gmail.com>
2021-08-20 12:13:19 +0530

    Vail Spreadsheet API Update

--- E/Epic/EpicPass_Whistler/js/content.js
+++ E/Epic/EpicPass_Whistler/js/content.js
@@ -2,29 +2,55 @@ var feedTemplate1 = [] = [];
 var feedContent = [], feedData = {}, getData;
 var adData = [];
 var loadTemplateFlag1 = false;
-var getFeed1 = function(){
-  var xmlhttp = new XMLHttpRequest();
-  // var url = "https://spreadsheets.google.com/feeds/list/19Bu_yWea-ZhvYr8ejUBolXn0J9LNFiJd5aqUv4DkTtQ/1/public/values?alt=json";
-  var url = "https://spreadsheets.google.com/feeds/list/1jeHHmS6rXEzWdqp7C3IENdGrZsFPxCGbESE1Yq3c83Y/4/public/values?alt=json";
+// var getFeed1 = function(){
+//   var xmlhttp = new XMLHttpRequest();
+//   // var url = "https://spreadsheets.google.com/feeds/list/19Bu_yWea-ZhvYr8ejUBolXn0J9LNFiJd5aqUv4DkTtQ/1/public/values?alt=json";
+//   var url = "https://spreadsheets.google.com/feeds/list/1jeHHmS6rXEzWdqp7C3IENdGrZsFPxCGbESE1Yq3c83Y/4/public/values?alt=json";
+
+//   xmlhttp.onreadystatechange = function() {
+//       if (this.readyState == 4 && this.status == 200) {
+//           var JSONData = JSON.parse(this.responseText);
+//           JSONData.feed.entry.map(function(data){
+//             feedTemplate1.push({
+//               "Ad Size": data['gsx$adsize']['$t'],
+//               "Layout": data['gsx$layout']['$t'],
+//               "Language": data['gsx$language']['$t'],
+//               "Resort": data['gsx$resortcode']['$t'],
+//               "Product": data['gsx$product']['$t'],
+//               "backgroundImage1": data['gsx$backgroundimage1']['$t'],
+//               "backgroundImage2": data['gsx$backgroundimage2']['$t'],
+//               "frameText1": data['gsx$frametext1']['$t'],
+//               "frameText2": data['gsx$frametext2']['$t'],
+//               "frameText3": data['gsx$frametext3']['$t'],
+//               "CTA": data['gsx$cta']['$t'],
+//               "clickURL": data['gsx$url']['$t'],
+//             });
+//           });
+//           loadTemplateFlag1 = true;
+//           loadData();
+//       }
+//   };
+//   xmlhttp.open("GET", url, true);
+//   xmlhttp.send();
+// }
+const getFeed1 = function(){
+  const xmlhttp = new XMLHttpRequest();
+  const API_KEY = "AIzaSyA9UwsLAgEsktyccelGlG_AV37qUCL-Gqo";
+  const sheetLocation = "1jeHHmS6rXEzWdqp7C3IENdGrZsFPxCGbESE1Yq3c83Y/Template_6_Whistler_EP_RT";
+  const searchId = location.search.split('?')[1];
+  const sheetId = searchId && searchId.length >= 44 && searchId.indexOf('/') > 1 ? searchId : sheetLocation;
+  const spreadsheetId = sheetId.split('/')[0];
+  const sheetName = sheetId.split('/')[1];
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}?key=${API_KEY}`;
 
   xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-          var JSONData = JSON.parse(this.responseText);
-          JSONData.feed.entry.map(function(data){
-            feedTemplate1.push({
-              "Ad Size": data['gsx$adsize']['$t'],
-              "Layout": data['gsx$layout']['$t'],
-              "Language": data['gsx$language']['$t'],
-              "Resort": data['gsx$resortcode']['$t'],
-              "Product": data['gsx$product']['$t'],
-              "backgroundImage1": data['gsx$backgroundimage1']['$t'],
-              "backgroundImage2": data['gsx$backgroundimage2']['$t'],
-              "frameText1": data['gsx$frametext1']['$t'],
-              "frameText2": data['gsx$frametext2']['$t'],
-              "frameText3": data['gsx$frametext3']['$t'],
-              "CTA": data['gsx$cta']['$t'],
-              "clickURL": data['gsx$url']['$t'],
-            });
+          const responseData = JSON.parse(this.responseText).values;
+          const headers = responseData[0], rows = responseData.slice(1);
+          rows.every(row => {
+            if(row.every(cell => cell === '')) return false; //isEmptyRow
+            feedTemplate1.push(row.reduce((obj, cell, i) => { obj[headers[i]] = cell; return obj; }, {}));
+            return true;
           });
           loadTemplateFlag1 = true;
           loadData();
@@ -70,12 +96,12 @@ var loadData = function(){
       obj.name = i;
       obj.data = [];
       var resortName = [];
-      feedData[i].map(function(data){resortName.push(data['Resort']);});
+      feedData[i].map(function(data){resortName.push(data['Resort Code']);});
       resortName = resortName.filter(function(value, index, self){ return self.indexOf(value) === index; })
       resortName.map(function(resortName){
         var smartObject = {};
         smartObject.name = resortName;
-        smartObject.data = feedData[i].filter(function(data){ return data['Resort'] == resortName });
+        smartObject.data = feedData[i].filter(function(data){ return data['Resort Code'] == resortName });
         obj.data.push(smartObject);
       });
       adData.push(obj);
