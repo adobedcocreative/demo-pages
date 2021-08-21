eceb8f8fd560c7fc6e40ffc4773358ed461f85ef
Neeraj Deshmukh <adobedcocreative@gmail.com>
2021-08-21 08:36:31 +0530

    Altice Update

--- A/Altice/2021/September/ads/OPT_1P_29(100 Mbps)/300x250/js/script.2.0.js
+++ A/Altice/2021/September/ads/OPT_1P_29(100 Mbps)/300x250/js/script.2.0.js
@@ -1,3 +1,12 @@
+//Polyfills
+if (!String.prototype.startsWith) {
+  Object.defineProperty(String.prototype, 'startsWith', {
+      value: function(search, rawPos) {
+          var pos = rawPos > 0 ? rawPos|0 : 0;
+          return this.substring(pos, pos + search.length) === search;
+      }
+  });
+}
 const textUpdate = function() {
   var textElements = ad.root.querySelectorAll('.text:not(.clip), .text-left:not(.clip), .text-right:not(.clip), .text-bottom:not(.clip), .text-top-left:not(.clip)');
   var clipTextElements = ad.root.querySelectorAll('.text.clip, .text-left.clip, .text-right.clip, .text-bottom.clip, .text-top-left.clip');
