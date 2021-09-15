e94e6cd1c92d871dfe363cfe15ff52039b4acac6
Neeraj Deshmukh <adobedcocreative@gmail.com>
2021-09-15 18:44:24 +0530

    Microsoft Update

---
+++ M/Microsoft/Microsoft_Office_Verde_Creative_Prospecting/ads/temp/300x250_AllUpAIScreens/copy.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+		
+	bannerData.headline1 = ["<#2f2f2f>Bring your |ideas to life","19px",14,40,"20","350", "left"];
+
+	bannerData.headline2 = ["<#737373>You got this. Smart |assistance features help |unleash your best work","11px",14.5,90,"13","350","left"];
+
+	bannerData.CTA = ["<#FFFFFF>LEARN MORE","12px",1,0,"50","300", "center"];
+	
+	bannerData.CTAarrowVisible = [true, 0,0]
