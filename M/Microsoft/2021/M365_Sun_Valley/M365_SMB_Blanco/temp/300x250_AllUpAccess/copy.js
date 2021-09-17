c869e7c6d7d1a5acb0bf1da5dc87e615b2982298
Neeraj Deshmukh <adobedcocreative@gmail.com>
2021-09-17 12:56:38 +0530

    Microsoft Update

---
+++ M/Microsoft/2021/M365_Sun_Valley/M365_SMB_Blanco/temp/300x250_AllUpAccess/copy.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+		
+	bannerData.headline1 = ["<#2f2f2f>Access your|business, anywhere","18px",15,41,"19","350", "left"];
+
+	bannerData.headline2 = ["<#737373>Connect with your team on|the fly and access files from|anywhere with 1 TB of OneDrive|cloud storage per user","11px",14.5,89,"14","350","left"];
+
+	bannerData.CTA = ["<#FFFFFF>BUY NOW","12px",1,0,"50","300", "center"];
+	
+	bannerData.CTAarrowVisible = [true, 0,0]
