081ef0fe2e4d5c985a7c5a6dc9d5c68d1d7c1613
Prateek Wahi <adobedcocreative@gmail.com>
2021-12-06 16:20:24 +0530

    AMO-146514
    M365 New Year themes updated

---
+++ M/Microsoft/2021/M365_New_Year_FY22/Desktop/ads/728x90/Excel/copy.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT, FONT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+	
+	bannerData.headline1 = ['<#505050>Budget for all of life’s','18.2px',466, 34,'18','350', 'center', 'Segoe Pro'];
+	bannerData.headline2 = ['<#505050>unexpected turns','18.2px',468, 34,'18','350', 'center', 'Segoe Pro'];
+	bannerData.headline3 = ['<#505050>Track this year’s finances in Excel','18.2px',469, 34,'18','350', 'center', 'Segoe Pro'];
+		
+	bannerData.CTA = ['<#ffffff>Learn more','9.2px',0,0,'50','300', 'left', 'Segoe Pro'];
+	
+	bannerData.CTAarrowVisible = [true, 0,0]
