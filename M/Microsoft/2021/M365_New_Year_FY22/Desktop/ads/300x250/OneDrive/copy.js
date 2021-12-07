4cc75d04d392cfb206e27f1f1064798c6557cd77
Prateek Wahi <adobedcocreative@gmail.com>
2021-12-07 09:17:56 +0530

    AMO-146514
    M365 DCO New Years

---
+++ M/Microsoft/2021/M365_New_Year_FY22/Desktop/ads/300x250/OneDrive/copy.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT, FONT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+	
+	bannerData.headline1 = ['<#505050>Journal, budget, passport,','18.2px',150, 58,'18','350', 'center', 'Segoe Pro'];
+	bannerData.headline2 = ['<#505050>driver’s license, and more','18.2px',150, 58,'18','350', 'center', 'Segoe Pro'];
+	bannerData.headline3 = ['<#505050>Safeguard private|documents in Personal Vault','18.4px',150, 46,'24','350', 'center', 'Segoe Pro'];
+		
+	bannerData.CTA = ['<#ffffff>Learn more','11.3px',0,0,'50','300', 'left', 'Segoe Pro'];
+	
+	bannerData.CTAarrowVisible = [true, 0,0]
---
+++ M/Microsoft/2021/M365_New_Year_FY22/Desktop/ads/300x600/Word/copy.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT, FONT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+	
+	bannerData.headline1 = ['<#505050>Chasing dreams','24px',150, 102,'18','350', 'center', 'Segoe Pro'];
+	bannerData.headline2 = ['<#505050>still takes|formatting','24px',150, 102,'26','350', 'center', 'Segoe Pro'];
+	bannerData.headline3 = ['<#505050>Be a bolder|you this year','24px',150, 102,'26','350', 'center', 'Segoe Pro'];
+		
+	bannerData.CTA = ['<#ffffff>Learn more','14.2px',0,0,'50','300', 'left', 'Segoe Pro'];
+	
+	bannerData.CTAarrowVisible = [true, 0,0]
