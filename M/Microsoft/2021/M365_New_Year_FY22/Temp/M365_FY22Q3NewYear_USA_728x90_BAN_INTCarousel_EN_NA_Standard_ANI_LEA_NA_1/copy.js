5eafd1424b8075d50d38fec5b0cf8462c844e48d
Prateek Wahi <adobedcocreative@gmail.com>
2021-12-10 11:15:04 +0530

    AMO-146514
    Added 728x90 to Desktop temp

---
+++ M/Microsoft/2021/M365_New_Year_FY22/Temp/M365_FY22Q3NewYear_USA_728x90_BAN_INTCarousel_EN_NA_Standard_ANI_LEA_NA_1/copy.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT, FONT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+		
+	bannerData.headline1 = ["Chase your dreams","18px",478,33,"24","350", "center", "Segoe Pro","#505050"];
+	bannerData.headline2 = ["Achieve the look","18px",478,33,"24","350", "center", "Segoe Pro","#505050"];
+	bannerData.headline3 = ["Boost your style","18px",478,33,"24","350", "center", "Segoe Pro","#505050"];
+	bannerData.headline4 = ["Track your goals","18px",478,33,"24","350", "center", "Segoe Pro","#505050"];
+	bannerData.headline5 = ["Keep private|info protected","18px",478,23,"24","350", "center", "Segoe Pro","#505050"];
+
+
+	bannerData.CTA = ["Learn more","9px",0,0,"12","300", "left", "Segoe Pro", "#FFFFFF"];
+	bannerData.CTAsize = [100,0,0];
+	bannerData.CTAarrowVisible = [true, 0,0]
+ 
