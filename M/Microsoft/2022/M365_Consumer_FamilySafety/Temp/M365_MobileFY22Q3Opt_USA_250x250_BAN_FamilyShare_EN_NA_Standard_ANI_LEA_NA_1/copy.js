dada64202fce2346385380bdd311ff4e8703e257
Prateek Wahi <adobedcocreative@gmail.com>
2022-01-05 14:35:41 +0530

    AMO-146912
    M365 Mobile Family Safety

---
+++ M/Microsoft/2022/M365_Consumer_FamilySafety/Temp/M365_MobileFY22Q3Opt_USA_250x250_BAN_FamilyShare_EN_NA_Standard_ANI_LEA_NA_1/copy.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+	//  TEXT, TEXTSIZE, XPOS, YPOS, LINE SPACING, LINE WIDTH, JUSTIFYTEXT, FONT
+	// use | for new line.  Use <#FFFFFF> for color change.
+	// when this occurs you can use line height to adjust line spacing.
+	// lineWidth is no longer needed.
+	var bannerData = new Object()
+	
+	bannerData.headline1 = ["<#505050>It's like 6 subscriptions in one","16.4px",125, 68,"18","350", "center", "Segoe Pro"];
+
+	bannerData.CTA = ["<#ffffff>Learn more","14px",0,0,"50","300", "left", "Segoe Pro"];
+	
+	bannerData.CTAarrowVisible = [true, 0,0]
